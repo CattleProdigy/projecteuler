a9da01ad32ff5e74d45aba9abd18491f77bfe50b
Paul Schroeder <paulschroeder90@gmail.com>
2017-03-25 21:16:52 -0600

    PE023: In Haskell, using "set comprehensions"

---
+++ pe023/main.hs
@@ -0,0 +1,27 @@
+import Debug.Trace
+import Data.Set hiding (filter)
+
+isDivisor :: Int -> Int -> Bool
+isDivisor x y = x `mod` y == 0
+
+listDivisors :: Int -> [Int]
+listDivisors x = filter (isDivisor x) candidates
+    where candidates = [1..x-1]
+
+isPerfect :: Int -> Bool
+isPerfect x = (sum $ listDivisors x) == x
+
+isAbundant :: Int -> Bool
+isAbundant x = (sum $ listDivisors x) > x
+
+isDeficient :: Int -> Bool
+isDeficient x = (sum $ listDivisors x) < x
+
+listAbundantNumbersLessThan :: Int -> [Int]
+listAbundantNumbersLessThan x = filter isAbundant [1..x]
+
+main = do putStrLn $ show $ sum $ toList nonSumsOfAbudants
+    where abundantNumbers = listAbundantNumbersLessThan 28123
+          sumsOfAbundantNumbersList = [x + y | x <- abundantNumbers, y <- abundantNumbers];
+          sumsOfAbundantNumbersSet = fromList sumsOfAbundantNumbersList
+          nonSumsOfAbudants = (fromList [1..28123]) \\ sumsOfAbundantNumbersSet
